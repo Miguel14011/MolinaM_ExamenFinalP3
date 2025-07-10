30b2e00a73e694e10814ec013531a4359f8acb21
MIguel14011 <jhonatan.molina@udla.edu.ec>
2025-07-10 07:40:04 -0500

    Creación de la estructura del proyecto, creación de modelo y definicion de la clase "Contacto"

---
+++ MolinaM_Examen/Models/Contacto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MolinaM_Examen.Models
+{
+    class Contacto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+
+        private string telefono;
+
+        public string Telefono
+        {
+            get => telefono;
+            set
+            {
+                if (!value.StartsWith("+593"))
+                    throw new ArgumentException("El teléfono debe iniciar con +593");
+                telefono = value;
+            }
+
+        }
+        public bool Favorito { get; set; }
+    }
+    
+}
